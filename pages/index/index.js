a74a51283cd3f367ad1a597cb92973f21e5824d8
Daisy-dan <wudan@yimei180.com>
2017-01-04 11:51:27 +0800

    update

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,18 +1,20 @@
 var flag = true;
+var color = 'window';
 Page({
     data:{
-        // text:"这是一个页面"
-        color:"window"
-    },
+        color : 'window'
+        },
     click:function () {
         console.log("点击了文字");
 
         if(flag){
-            color = "window";
+            color= 'window';
             flag = false;
+            console.log(color);
         }else{
-            color = "window-red";
+            color= 'window-red';
             flag = true;
+            console.log(color);
         }
         this.setData({
             color
